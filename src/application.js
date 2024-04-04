ee63067da9bcfa4ccd352f39f8365b4baac926c6
Andrei Belskii <mrandrewer@yandex.ru>
2024-04-05 00:22:43 +0300

    Second task

--- src/application.js
+++ src/application.js
@@ -1,3 +1,12 @@
+import axios from 'axios';
+import onChange from 'on-change';
+
+const routes = {
+  usersPath: () => '/users',
+};
+
+const netWorkError = () => 'Network Problems. Try again.';
+
 const renderForm = (container) => {
   const form = document.createElement('form');
   form.id = 'registrationForm';
@@ -10,12 +19,87 @@ const renderForm = (container) => {
   <input type="text" class="form-control" id="inputEmail" placeholder="Введите email" name="email" required>
 </div>
 <input type="submit" value="Submit" class="btn btn-primary">`;
+
   container.replaceChildren(form);
+  return form;
+};
+
+const renderSuccess = (elements, state) => {
+  const p = document.createElement('p');
+  p.textContent = state.form.response;
+  elements.container.replaceChildren(p);
+};
+
+const handleFormState = (elements, state) => {
+  switch (state.form.processState) {
+    case 'success':
+      renderSuccess(elements, state);
+      break;
+    default:
+      break;
+  }
 };
 
 const app = () => {
   const container = document.querySelector('.form-container');
-  renderForm(container);
+  const form = renderForm(container);
+
+  const state = {
+    form: {
+      processState: '',
+      field: {
+        name: '',
+        email: '',
+      },
+      response: '',
+    },
+  };
+
+  const elements = {
+    container,
+    inputName: document.getElementById('inputName'),
+    inputEmail: document.getElementById('inputEmail'),
+    form,
+  };
+
+  const watchedState = onChange(state, (path) => {
+    switch (path) {
+      case 'form.processState':
+        handleFormState(elements, watchedState);
+        break;
+      default:
+        break;
+    }
+  });
+
+  elements.inputName.addEventListener('input', (e) => {
+    e.preventDefault();
+    watchedState.form.processState = 'filling';
+    const { value } = e.target;
+    watchedState.form.field.name = value.trim();
+  });
+
+  elements.inputEmail.addEventListener('input', (e) => {
+    e.preventDefault();
+    watchedState.form.processState = 'filling';
+    const { value } = e.target;
+    watchedState.form.field.email = value.trim();
+  });
+
+  elements.form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    state.form.processState = 'sending';
+    try {
+      const response = await axios.post(routes.usersPath(), watchedState.form.field);
+      watchedState.form.response = response.data.message;
+      watchedState.form.field.name = '';
+      watchedState.form.field.email = '';
+      watchedState.form.processState = 'success';
+    } catch (err) {
+      watchedState.form.processState = 'error';
+      watchedState.form.processError = netWorkError();
+    }
+  });
 };
 
 export default app;
